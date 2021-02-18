e111cf10b10389f7587b23c90801944a65b1bfcb
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-02-18 22:56:34 +0900

    token

---
+++ encryption/encryption.go
@@ -0,0 +1,68 @@
+package encryption
+
+import (
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+	"time"
+
+	"github.com/lemon-mint/challenge-server/token"
+	"google.golang.org/protobuf/proto"
+)
+
+//Packer : sign encrypt verify decrypt
+type Packer struct {
+	key  []byte
+	aead cipher.AEAD
+}
+
+//NewToken : Create New token
+func (p *Packer) NewToken(exp time.Duration, id string) ([]byte, error) {
+	buf := make([]byte, 8)
+	io.ReadFull(rand.Reader, buf)
+	t := &token.AccessToken{
+		Timestamp: time.Now().UTC().Unix(),
+		Expire:    time.Now().UTC().Add(exp).Unix(),
+		Id:        id,
+		Nonce:     buf,
+	}
+	nonce := make([]byte, p.aead.NonceSize())
+	io.ReadFull(rand.Reader, nonce)
+	msg, err := proto.Marshal(t)
+	if err != nil {
+		return nil, err
+	}
+	encrypted := p.aead.Seal(nil, nonce, msg, nil)
+	buf = make([]byte, len(encrypted)+p.aead.NonceSize())
+	copy(buf[:p.aead.NonceSize()], nonce)
+	copy(buf[p.aead.NonceSize():], encrypted)
+	return buf, nil
+}
+
+//Verify : token
+func (p *Packer) Verify(tokenString string, id string) bool {
+	encrypted, err := base64.RawStdEncoding.DecodeString(tokenString)
+	if err != nil {
+		return false
+	}
+	if len(encrypted) <= p.aead.NonceSize() {
+		return false
+	}
+	msg, err := p.aead.Open(nil, encrypted[:p.aead.NonceSize()], encrypted[p.aead.NonceSize():], nil)
+	if err != nil {
+		return false
+	}
+	t := new(token.AccessToken)
+	err = proto.Unmarshal(msg, t)
+	if err != nil {
+		return false
+	}
+	if t.Expire >= time.Now().Unix() {
+		return false
+	}
+	if t.Id != id {
+		return false
+	}
+	return true
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lemon-mint/challenge-server
+
+go 1.16
+
+require google.golang.org/protobuf v1.25.0
